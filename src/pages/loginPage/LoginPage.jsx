e58aaf5d6ef2f43c90a10dda4ac3b4bcd5fe2ccd
Muhammad-N-Ali <m.naeemali572@gmail.com>
2024-06-01 22:25:30 +0200

    feature/login branch

--- src/pages/loginPage/LoginPage.jsx
+++ src/pages/loginPage/LoginPage.jsx
@@ -1,13 +1,39 @@
+
+import React from "react";
 import "./LoginPage.css";
+
 
 const LoginPage = () => {
   return (
     <main>
-      <section>
-        <h1> Welcome to Your Account </h1>
-      </section>
+      <div className="flex w-full h-screen">
+        <div className="w-full flex flex-col items-center justify-center">
+          <h1 className="text-3xl font-semibold">Welcome to myCar</h1>
+          <div className=" bg-blue-900 text-white border-2 rounded-3xl p-10 mt-8">
+            <form  action="">
+              <div className="mt-8">
+                <label htmlFor="">User Name:</label>
+                <input className="w-full border-2 border-gray-100 rounded-lg p-2 text-sm" type="text" placeholder="Enter your Name"/>
+              </div>
+                
+              <div className="mt-4">
+                <label htmlFor="">Password:</label>
+                <input className="w-full border-2 border-gray-100 rounded-lg p-2 text-sm" type="Password" placeholder="Enter your password "/>
+              </div>
+
+              <button className="w-full mt-20 border-2 rounded-3xl px-10 py-2 bg-orange-400">Sign In</button>
+            </form>
+          </div>
+
+          <div className="flex justify-between text-sm mt-2">
+              <p className="">Do not have an account? Sign Up </p>
+              <button className=""> Forget Password</button>
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
+
 
 export default LoginPage;
--- src/pages/signupPage/SignUpPage.jsx
+++ src/pages/signupPage/SignUpPage.jsx
@@ -1,11 +1,34 @@
+// import React from "react";
 import "./SignUpPage.css";
 
 const SignUpPage = () => {
   return (
     <main>
-      <section>
-        <h1> Create Account for Free </h1>
-      </section>
+      <div className="">
+        <h3>Create Your Free Account</h3>
+        <div className="mt-4">
+          <form action="">
+            <div>
+              <label htmlFor="">User Name</label>
+              <input type="text" placeholder="Write Your Full Name"/>
+              </div>
+              <div>
+              <label htmlFor=""> Email</label>
+              <input type="email" placeholder="Write Your Email Address"/>
+              </div>
+              <div>
+              <label htmlFor="">Password</label>
+              <input type="Password" />
+              </div>
+              <div>
+              <label htmlFor="">Repeat Password</label>
+              <input type="password" />
+              </div>
+          </form>
+          <button>Sign Up</button>
+        </div>
+        
+      </div>
     </main>
   );
 };
