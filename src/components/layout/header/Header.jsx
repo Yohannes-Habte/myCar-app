453f8ee163b6a4462483125e200d75b8924de53a
mdMehediBillah <mailbillah@gmail.com>
2024-06-01 23:49:40 +0200

    backend data fetched

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,11 +1,16 @@
 import { Link, NavLink } from "react-router-dom";
 import "./Header.css";
-import { FaShoppingCart } from "react-icons/fa";
+import { FaShoppingCart, FaCar, FaUserCircle } from "react-icons/fa";
 
 const Header = () => {
   return (
     <header className="flex justify-between px-10 py-3 bg-neutral-900 text-white">
-      <div>logo</div>
+      <Link to="/">
+        <div className="logo-header flex gap-2 items-center sm:justify-center md:justify-start">
+          <FaCar />
+          <div className="logo-header">myCar</div>
+        </div>
+      </Link>
 
       <nav className="flex space-x-10 items-center">
         <ul className="flex space-x-5 ">
@@ -21,6 +26,14 @@ const Header = () => {
         </ul>
 
         <ul className="flex space-x-5 items-center ">
+          <li>
+            <Link to={"/login"}>
+              <div className="flex items-center">
+                <FaUserCircle />
+                <div className="px-1">User</div>
+              </div>
+            </Link>{" "}
+          </li>
           <li className="cart-item">
             <NavLink to={"/cart"}>
               <FaShoppingCart />
